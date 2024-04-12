c10d3d8edb40ba5cd91e1ace8ce5774620b979bc
Vladimir Andrianov <vladimir.rostok96@gmail.com>
2024-04-12 03:04:30 +0300

    Add current GREEN token balance section to Dashboard wallet info

--- src/app/_components/useElvenDapp/demo/get-user-data-demo.tsx
+++ src/app/_components/useElvenDapp/demo/get-user-data-demo.tsx
@@ -5,10 +5,46 @@ import Link from 'next/link';
 import { useAccount, useConfig } from '@useelven/core';
 import { Card, CardContent } from '../ui/card';
 import { shortenHash } from '../lib/shorten-hash';
+import { useEffect, useState } from 'react';
 
 export const GetUserDataDemo = () => {
   const { address, nonce, balance, activeGuardianAddress } = useAccount();
   const { explorerAddress } = useConfig();
+
+  // Fetch GREEN token balance for the current wallet session.
+  const [balanceGreen, setBalanceGreen] = useState<number>();
+  useEffect(() => {
+    const getGreenBalance = async () => {
+      const res = await fetch(
+        'https://api.multiversx.com' +
+          `/accounts/` +
+          address +
+          `/tokens?name=GREEN`,
+        {
+          method: 'GET',
+        },
+      );
+
+      if (!res.ok) {
+        return console.error(await res.text());
+      }
+
+      if (res.status === 200) {
+        const data = await res.json();
+
+        // Check if response is empty.
+        if (data[0] && data[0]?.balance) {
+          let balanceGreen = data[0].balance;
+
+          if (balanceGreen != 0) {
+            setBalanceGreen(balanceGreen);
+          }
+        }
+      }
+    };
+
+    getGreenBalance();
+  }, [address]);
 
   return (
     <Card className="flex-1">
@@ -44,12 +80,16 @@ export const GetUserDataDemo = () => {
           <span className="inline-block font-bold">nonce:</span> {nonce}
         </div>
         <div>
-          <span className="inline-block font-bold">balance:</span>{' '}
+          <span className="inline-block font-bold">EGLD balance:</span>{' '}
           {balance
             ? parseFloat(
                 TokenTransfer.egldFromBigInteger(balance).toPrettyString(),
               )
             : '-'}
+        </div>
+        <div>
+          <span className="inline-block font-bold">GREEN balance:</span>{' '}
+          {balanceGreen ? balanceGreen : '-'}
         </div>
       </CardContent>
     </Card>
