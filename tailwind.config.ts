0744f3bea577c3470c110e76a705787219149f57
Vladimir <vladimir.rostok96@gmail.com>
2024-07-23 03:44:37 +0300

    Clean up the tailwind config from the MvX project usage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,11 @@ const config: Config = {
   theme: {
     extend: {
       boxShadow: {
-        glow: '0 0 8px 2px rgba(252, 211, 77, 0.6)',
         input:
           '0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)',
       },
       fontFamily: {
         'philosopher-regular': ['Philosopher', 'sans-serif'],
-      },
-      fontSize: {
-        tiny: '0.75rem', // 12px
-        huge: '3rem', // 48px
       },
       screens: {
         '4k': '2560px', // New breakpoint for 4K screens.
@@ -23,19 +18,15 @@ const config: Config = {
         'colr-ghostverse-teal': '#0080e4',
         'colr-greenghost-teal': '#13de00',
         'colr-l-primary': '#b4b4b4',
-        'colr-l-secondary': '#284d5a',
-        'colr-d-primary': '#475569',
-        'colr-d-secondary': '#D1D5DB',
+        'colr-l-secondary': '#284d5a', // unused
+        'colr-d-primary': '#475569', // unused
+        'colr-d-secondary': '#D1D5DB', // unuse
         'colr-l-fg': '#dadada',
         'colr-l-bg': '#000000',
         'colr-d-fg': '#ccceef',
         'colr-d-bg': '#070710',
         'colr-d-btn': '#070710',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
