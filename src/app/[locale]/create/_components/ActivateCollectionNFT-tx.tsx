f4343559716c6886797c3133b54f9dc259a01c8b
Vladimir Rostok <vladimir.rostok96@gmail.com>
2024-08-02 02:41:39 +0300

    Refactor & implement the "Activate Collection" step

--- src/app/[locale]/create/_components/ActivateCollectionNFT-tx.tsx
+++ src/app/[locale]/create/_components/ActivateCollectionNFT-tx.tsx
@@ -1,14 +1,14 @@
+'use client';
+
 import {
   TransactionCallbackParams,
   useAccount,
   useLoggingIn,
   useTransaction,
+  useApiCall,
+  SCQueryType,
+  useConfig,
 } from '@useelven/core';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from '../../../_components/port/useElvenDapp/ui/card';
 import { Button } from '../../../_components/port/useElvenDapp/ui/button';
 import {
   StringValue,
@@ -17,6 +17,7 @@ import {
   ContractCallPayloadBuilder,
 } from '@multiversx/sdk-core';
 import { TokenTransfer, Address } from '@multiversx/sdk-core';
+import { useEffect, useState } from 'react';
 
 export const ActivateCollectionNFT = ({
   cb,
@@ -27,13 +28,20 @@ export const ActivateCollectionNFT = ({
   const { address } = useAccount();
   const { loggedIn } = useLoggingIn();
 
-  const handleSendTx = () => {
-    const fullTokenIdentifier = 'TEST-2be25e';
+  // Non cretable collections yet.
+  const [collectionTicker, setCollectionTicker] = useState<string>('');
 
+  const { data: collections, isLoading: collectionsLoading } = useApiCall<
+    any[]
+  >({
+    url: `/accounts/${address}/roles/collections?type=NonFungibleESDT&canCreate=false`,
+  });
+
+  const handleSendTx = () => {
     const transactionPayload = new ContractCallPayloadBuilder()
       .setFunction(new ContractFunction('setSpecialRole'))
       .setArgs([
-        new StringValue(fullTokenIdentifier),
+        new StringValue(collectionTicker),
         new AddressValue(Address.fromBech32(address)), // Current ElvenTools doesn't provide method for proper conversion.
         new StringValue('ESDTRoleNFTCreate'),
       ])
@@ -48,34 +56,60 @@ export const ActivateCollectionNFT = ({
   };
 
   return (
-    <Card className="flex flex-1 flex-col justify-between">
-      <CardContent className="mt-6">
-        <div className="mb-4">
-          {`2. You will be setting a special ESDTRoleNFTCreate role to the token you just created to create NFT-s on top of that "collection" token.`}
-          <br />
+    <form className="flex flex-col gap-y-4">
+      <div className="sm:mt-4">
+        <div className="text-center text-md text-gray-300 font-bold">
+          {'Activate an existing collection token to create NFTs on top of it'}
         </div>
-      </CardContent>
-      <CardFooter>
+        <div>
+          {collections && collections.length > 0 ? (
+            <select
+              name="collections"
+              id="collection-select"
+              value={collectionTicker}
+              className="w-full inline-flex mt-5 text-base bg-gray-900 hover:bg-sky-500 focus:bg-gray-700 items-center p-4 rounded-full"
+              required
+              onChange={(e) => {
+                setCollectionTicker(e.target.value);
+              }}
+            >
+              <option
+                disabled
+                selected
+                hidden
+                key={''}
+                value={''}
+                label="Choose a collection"
+              />
+              {collections.map((item) => (
+                <option
+                  key={item.ticker}
+                  value={item.ticker}
+                  label={item.name + ' - ' + item.ticker}
+                >
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <div className="w-full text-center cursor-not-allowed text-red-400 mt-4 text-base bg-gray-900 focus:bg-gray-700 items-center p-4 rounded-full">
+              Create new collection first!
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="flex justify-center">
         <Button
-          variant="outline"
-          disabled={pending || !loggedIn}
+          variant="ghostTheme"
+          size={'lg'}
+          className="!w-1/2"
+          disabled={pending || !loggedIn || !collectionTicker}
           onClick={handleSendTx}
         >
           Activate
         </Button>
-      </CardFooter>
-    </Card>
+      </div>
+    </form>
   );
-};
-
-export const stringToHex = (str: string) => {
-  if (str) {
-    const arr1 = [];
-    for (let n = 0, l = str.length; n < l; n++) {
-      const hex = Number(str.charCodeAt(n)).toString(16);
-      arr1.push(hex);
-    }
-    return arr1.join('');
-  }
-  return '';
 };
